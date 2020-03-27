bc65d03580a93096cccc89973419319eb895b195
winalotobrother <62186428+winalotobrother@users.noreply.github.com>
2020-03-27 13:30:48 +0300

    Made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+    } else {
+        document.getElementById('cool').className = 'cool';
+    }
+}
+
+
+
+
+
+
 var sayMyName = function (name) {
     alert('My name is: '+name); 
 }
+
 
 var car = {
     make: 'VW',
